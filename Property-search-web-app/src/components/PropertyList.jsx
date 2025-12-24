8db6af852842d51cbc223d9c81368ff4c92bfe46
Wathila Heshan <heshang416@gmail.com>
2025-12-24 19:15:29 +0530

    Add property list component

---
+++ Property-search-web-app/src/components/PropertyList.jsx
@@ -0,0 +1,19 @@
+import { PropertyCard } from "./PropertyCard.jsx";
+
+export function PropertyList({properties, onFavourite}){
+    if(!properties.length){
+        return <p>No properties match your search</p>
+    }
+
+    return(
+        <div className="property-list">
+            {properties.map((p) => (
+                <PropertyCard
+                    key={p.id}
+                    property={p}
+                    onFavourite={onFavourite}
+                />
+            ))}
+        </div>
+    );
+}
