6dc5f9de3dd76409a1f1ad87564c3a92886606cc
Wathila Heshan <heshang416@gmail.com>
2025-12-29 23:18:54 +0530

    configure project for GitHub Pages deployment

--- Property-search-web-app/src/main.jsx
+++ Property-search-web-app/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./App.jsx";
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* Wrap app with router for navigation */}
-    <BrowserRouter><App /></BrowserRouter>
+    <HashRouter><App /></HashRouter>
   </StrictMode>,
 )
